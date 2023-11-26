6e2b0a5031e8be00a9d91a28598ec7f61cc88b9c
jeronima <jeronima.floriano@totvs.com.br>
2023-11-26 10:49:26 -0300

    Ajustando icones de redes sociais

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -5,6 +5,8 @@ import { ProjectsComponent } from './projects/projects.component';
 import { ContactComponent } from './contact/contact.component';
 
 const routes: Routes = [
+  { path: '', component: AppComponent },
+  { path: '**', component: AppComponent },
   { path: 'contato', component: ContactComponent },
   { path: 'projetos', component: ProjectsComponent },
 ];
